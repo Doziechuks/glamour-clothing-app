76cd5111ba40b41c7c20ecbed70e814f90d668de
doziechuks <doziechuks1010@gmail.com>
2022-10-04 23:10:52 +0100

    added more styles to my navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,22 +1,46 @@
 import './navBar.css';
 import { BsFillBrightnessHighFill } from "react-icons/bs";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [active, setActive] = useState('home');
   return (
     <div className="nav-wrapper">
-      <BsFillBrightnessHighFill className="nav-icon" />
+      <Link
+       to="/"
+       onClick={() => setActive("home")} 
+       className="logo-link"
+       >
+        <BsFillBrightnessHighFill className="nav-icon" />
+      </Link>
       <div className="pages">
-        <Link to="/" className="option">
+        <Link
+          to="/"
+          onClick={() => setActive("home")}
+          className={`option ${active === "home" ? "active" : ""}`}
+        >
           Home
         </Link>
-        <Link to="/shop" className="option">
+        <Link
+          to="/shop"
+          onClick={() => setActive("shop")}
+          className={`option ${active === "shop" ? "active" : ""}`}
+        >
           shop
         </Link>
-        <Link to="/contact" className="option">
+        <Link
+          to="/contact"
+          onClick={() => setActive("contact")}
+          className={`option ${active === "contact" ? "active" : ""}`}
+        >
           contact us
         </Link>
-        <Link to="/sign in" className="option">
+        <Link
+          to="/sign in"
+          onClick={() => setActive("sign in")}
+          className={`option ${active === "sign in" ? "active" : ""}`}
+        >
           sign in
         </Link>
       </div>
