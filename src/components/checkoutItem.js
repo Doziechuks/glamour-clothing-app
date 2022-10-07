f8ba9383aaf682e478cf888b9c5d52ed6c759828
doziechuks <doziechuks1010@gmail.com>
2022-10-08 00:20:24 +0100

    added responsiveness to the check out page

--- src/components/checkoutItem.js
+++ src/components/checkoutItem.js
@@ -11,6 +11,11 @@ const CheckoutItem = ({ cartItem }) => {
       <p className="num">{quantity}</p>
       <p className="price">${price}</p>
       <div className="remove-button">&#10006;</div>
+        {/* for mobile view */}
+      <p className="name for-mobile">Description: {name}</p>
+      <p className="num for-mobile">Quantity: {quantity}</p>
+      <p className="price for-mobile">Price: ${price}</p>
+      <div className="remove-button for-mobile">Remove: &#10006;</div>
     </div>
   );
 }
