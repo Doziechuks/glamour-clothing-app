c0b757767727b2326336911c31c03c062160f1a2
doziechuks <doziechuks1010@gmail.com>
2022-10-08 23:15:10 +0100

    added redux-persist and used redux persistor to store cartItem to local storage

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import store from "./redux/store";
+import { store, persist } from "./redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,7 +14,9 @@ root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persist}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,17 @@
 import { combineReducers } from "redux";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 import userReducer from "./userReducer";
 import cartReducer from "./cartReducer";
 
-export default combineReducers({
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+}
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+export default persistReducer(persistConfig, rootReducer);
